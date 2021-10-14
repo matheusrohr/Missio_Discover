c3814c7693c8aa8670aa7dec93518990717705cf
matheusrohr <matheus.rohr@gmail.com>
2021-10-14 14:44:37 -0300

    commit dia 14102021 inicio do Node

--- src/route.js
+++ src/route.js
@@ -3,3 +3,9 @@ const express = require('express')
 const route = express.Router()
 
 route.get('/', (req, res) => res.render("index"))
+
+route.get('/room', (req, res) => res.render("room"))
+
+route.get('/creat-pass', (req, res) => res.render("creat-pass"))
+
+module.exports = route
