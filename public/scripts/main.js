077ec3d5c9a550c3a278fb8f83532f6261aacf0c
matheusrohr <matheus.rohr@gmail.com>
2021-10-14 17:45:44 -0300

    segundo commit dia 14102021

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,10 +3,25 @@ import Modal from './modal.js'
 const modal = Modal()
 
 const checkButtons = document.querySelectorAll(".actions a.check")
+const deleteButtons = document.querySelectorAll(".actions a.delete")
+const modalTitle = document.querySelector(".modal H2")
+const modalDescription = document.querySelector(".modal p")
+const modalButton = document.querySelector(".modal form .buttons button")
 
 checkButtons.forEach(button => {
-    
-    button.addEventListener("click", event => {
-        modal.open()
-    })
+    button.addEventListener("click", handleClick)
 })
+
+deleteButtons.forEach(button => {
+    button.addEventListener("click", (event) => handleClick(event, false))
+})
+
+function handleClick(event, check = true){
+    event.preventDefault()
+    const text = check ? "Marcar como lida" : "Excluir"
+    modalTitle.innerHTML = `${text} esta pergunta`
+    modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} está pergunta`
+    modalButton.innerHTML = `Sim, ${text}`
+    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+    modal.open()
+}
--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,8 +1,16 @@
 export default function Modal(){
+    
+    const cancelButton = document.querySelector('.button.cancel')
+    const modalWrapper = document.querySelector('.modal-wrapper')
+
+    cancelButton.addEventListener("click", close)
+
     function open(){
-        document.querySelector('.modal-wrapper').classList.add("active")
+        modalWrapper.classList.add("active")
     }
-    function close(){}
+    function close(){
+        modalWrapper .classList.remove("active")
+    }
 
     return{
         open,
